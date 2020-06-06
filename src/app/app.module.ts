5e1a13ff422314bf37a7e81f9ca6e2b12798b9c7
John Adler <bigjca@gmail.com>
2020-06-05 23:12:42 -0400

    finished tutorial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,43 @@
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { faExternalLinkAlt, faUserCircle } from '@fortawesome/free-solid-svg-icons';
+import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HomeComponent } from './home/home.component';
+import { AlertsComponent } from './alerts/alerts.component';
+import { MsalModule } from '@azure/msal-angular';
+import { OAuthSettings } from '../oauth';
+import { CalendarComponent } from './calendar/calendar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    HomeComponent,
+    AlertsComponent,
+    CalendarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    FontAwesomeModule,
+    MsalModule.forRoot({
+      auth: {
+        clientId: OAuthSettings.appId
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(library: FaIconLibrary) {
+    // Register the FontAwesome icons used by the app
+    library.addIcons(faExternalLinkAlt, faUserCircle);
+  }
+ }
